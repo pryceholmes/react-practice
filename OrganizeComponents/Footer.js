03af5095e6adaa4b93fb0c8f0af5e5da668a4012
pryceholmes <113060481+pryceholmes@users.noreply.github.com>
2024-11-07 19:51:37 -0600

    more react tutorials! Went through seperating custom components and basic styling.

---
+++ OrganizeComponents/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Footer() {
+    return (
+        <footer>
+            <small>© 2024 Holmes development. All rights reserved.</small>
+        </footer>
+    )
+}
